683a870944b2febd8760d9a0e5f7c0fb327559bb
Ezequiel-UNI <73581792+Ezequiel-UNI@users.noreply.github.com>
2021-02-09 15:07:50 +0100

    Generación automática del fichero con Eclipse

---
+++ src/test/java/DecoratorPattern/CheeseTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CheeseTest {
+
+	@Test
+	void getDesc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void getPrice() {
+		fail("Not yet implemented");
+	}
+
+}
